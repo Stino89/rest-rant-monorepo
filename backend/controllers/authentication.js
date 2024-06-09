0a44a88e59e430da769edd73014d93a55b1e277b
Faustino Ruiz <ftr1989@gmail.com>
2024-06-09 07:39:36 -0700

    iam-4-jwts

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -3,26 +3,33 @@ const db = require("../models")
 const bcrypt = require('bcrypt')
 
 const { User } = db
+
 router.post('/', async (req, res) => {
-    
+
     let user = await User.findOne({
-        where: { 
-            email: req.body.email          
-        }
+        where: { email: req.body.email }
     })
 
     if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
-        res.status(404).json({ 
-            message: `Could not find a user with the provided username and password` 
-        })
+        res.status(404).json({ message: `Could not find a user with the provided username and password` })
     } else {
+        req.session.userId = user.userId
+        console.log(req)
         res.json({ user })
     }
 })
 
-
-
-  
-
+router.get('/profile', async (req, res) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                userId: 1
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
 
 module.exports = router
